6509aa8ec267fe9679d8fe843087084947aaa192
Võ Tấn Thành <votanthanh32004@gmail.com>
2026-06-13 16:16:24 +0700

    update: change flow. Upload image first in frontend (get image url). After done then create product.

--- src/pages/seller/api/index.js
+++ src/pages/seller/api/index.js
@@ -2,3 +2,5 @@ export { default as sellerProductApi } from "./sellerProductApi";
 export { default as sellerOrderApi } from "./sellerOrderApi";
 export { default as sellerStatisticApi } from "./sellerStatisticApi";
 export { default as sellerCategoryApi } from "./sellerCategoryApi";
+export { default as imageApi } from "./imageApi";
+
